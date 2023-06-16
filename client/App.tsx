3366693bbd4ad9c1acb3fc9855a865cd629f268b
atilgner <aidantilgner02@gmail.com>
2023-06-16 08:05:15 -0700

    feat: initial commit

---
+++ client/App.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+function App() {
+  const [count, setCount] = React.useState(0);
+
+  return (
+    <div>
+      <p>I am a basic React app :)</p>
+      <p>
+        For more info, check out{" "}
+        <a href="https://github.com/AidanTilgner/App-Template.git">this repo</a>
+        .
+      </p>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        {count > 0 ? `I have been clicked ${count} times` : "Click me!"}
+      </button>
+    </div>
+  );
+}
+
+export default App;
---
+++ esbuild.dev.config.mjs
@@ -0,0 +1,66 @@
+import * as esbuild from "esbuild";
+import { sassPlugin, postcssModules } from "esbuild-sass-plugin";
+import { config } from "dotenv";
+import http from "http";
+
+config();
+
+const watch = async () => {
+    const ctx = await esbuild.context({
+        entryPoints: ["client/index.tsx"],
+        bundle: true,
+        outfile: "public/build/bundle.js",
+        sourcemap: true,
+        minify: false,
+        plugins: [
+            sassPlugin({
+                filter: /\.module\.scss$/,
+                transform: postcssModules({
+                    generateScopedName: "[name]__[local]___[hash:base64:5]",
+                    basedir: "client",
+                }),
+                type: "css",
+            }),
+            sassPlugin({
+                filter: /\.scss$/,
+            }),
+            {
+                name: "development-server",
+                setup: (build) => {
+                    build.onEnd(() => {
+                        const now = new Date().toLocaleTimeString();
+                        console.info(`Build finished at ${now}. Watching for changes...`);
+                        // make post request to /esbuild-rebuilt
+                        const req = http.request(
+                            {
+                                host: "localhost",
+                                port: process.env.PORT,
+                                path: "/esbuild-rebuilt",
+                                method: "POST",
+                            },
+                            () => {
+                                console.info("Sent rebuild notification...");
+                            }
+                        );
+                        req.on("error", (err) => {
+                            console.error("Error sending rebuild notification:", err);
+                        });
+                        req.end();
+                    });
+                },
+            },
+        ],
+        loader: {
+            ".js": "jsx",
+            ".jsx": "jsx",
+            ".ts": "tsx",
+            ".tsx": "tsx",
+            ".scss": "css",
+        },
+        jsxFactory: "React.createElement",
+        jsxFragment: "React.Fragment",
+    });
+    await ctx.watch();
+    console.info("Served and Watching...");
+};
+watch();
