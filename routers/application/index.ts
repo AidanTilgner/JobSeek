f74835eb9cd38814d85545a2f3745ac3bda5f1eb
atilgner <aidantilgner02@gmail.com>
2023-06-18 13:59:06 -0700

    feat: started adding routes

--- routers/application/index.ts
+++ routers/application/index.ts
@@ -1,10 +1,18 @@
 import { Router } from "express";
 import { createCoverLetter, createCoverLetterStream } from "./handlers";
 import { getSocket } from "../../utils/socket.io";
+import { checkAccess } from "../../middleware/auth";
+import {
+  newResume,
+  deleteResume,
+  updateResume,
+  addSkillToResume,
+} from "../../database/functions/resume";
+import { User } from "../../database/models/user";
 
 const router = Router();
 
-router.post("/new/cover-letter", (req, res) => {
+router.post("/new/cover-letter", checkAccess, (req, res) => {
   try {
     const socket = getSocket();
 
@@ -28,6 +36,158 @@ router.post("/new/cover-letter", (req, res) => {
     res.status(500).json({
       success: false,
       message: "Something went wrong.",
+    });
+  }
+});
+
+router.post("/new/resume", checkAccess, async (req, res) => {
+  try {
+    const { name, description } = req.body;
+
+    if (!name || !description) {
+      return res.status(400).json({
+        success: false,
+        message: "Missing required fields.",
+      });
+    }
+
+    const user = (
+      req as unknown as {
+        jwtPayload: unknown;
+      }
+    ).jwtPayload;
+
+    const resume = await newResume((user as User).id, {
+      name,
+      description,
+    });
+
+    return res.status(200).json({
+      success: true,
+      message: "Resume created.",
+      data: resume,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      success: false,
+      message: "Something went wrong.",
+      data: null,
+    });
+  }
+});
+
+router.put("/resume/:resume_id", checkAccess, async (req, res) => {
+  try {
+    const { name, description } = req.body;
+    const { resume_id } = req.params;
+
+    if (!resume_id || !name || !description) {
+      return res.status(400).json({
+        success: false,
+        message: "Missing required fields.",
+      });
+    }
+
+    const user = (
+      req as unknown as {
+        jwtPayload: unknown;
+      }
+    ).jwtPayload;
+
+    const parsedResumeID = Number(resume_id);
+
+    const resume = await updateResume((user as User).id, parsedResumeID, {
+      name,
+      description,
+    });
+
+    return res.status(200).json({
+      success: true,
+      message: "Resume updated.",
+      data: resume,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      success: false,
+      message: "Something went wrong.",
+      data: null,
+    });
+  }
+});
+
+router.delete("/resume/:resume_id", checkAccess, async (req, res) => {
+  try {
+    const { resume_id } = req.params;
+
+    if (!resume_id) {
+      return res.status(400).json({
+        success: false,
+        message: "Missing required fields.",
+      });
+    }
+
+    const user = (
+      req as unknown as {
+        jwtPayload: unknown;
+      }
+    ).jwtPayload;
+
+    const parsedResumeID = Number(resume_id);
+
+    await deleteResume((user as User).id, parsedResumeID);
+
+    return res.status(200).json({
+      success: true,
+      message: "Resume deleted.",
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      success: false,
+      message: "Something went wrong.",
+    });
+  }
+});
+
+router.post("/resume/:resume_id/skill", checkAccess, async (req, res) => {
+  try {
+    const { resume_id } = req.params;
+    const { name, description, level } = req.body;
+
+    if (!resume_id || !name || !description || !level) {
+      return res.status(400).json({
+        success: false,
+        message: "Missing required fields.",
+      });
+    }
+
+    const user = (
+      req as unknown as {
+        jwtPayload: unknown;
+      }
+    ).jwtPayload;
+
+    const parsedResumeID = Number(resume_id);
+
+    const skill = await addSkillToResume((user as User).id, parsedResumeID, {
+      name,
+      description,
+      level,
+    });
+
+    return res.status(200).json({
+      success: true,
+      message: "Skill added.",
+      data: skill,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      success: false,
+      message: "Something went wrong.",
+      data: null,
     });
   }
 });
